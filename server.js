00a563799c2896d22269cbf658e1e4eec0599b02
Matthé <matthevdberg@gmail.com>
2023-04-20 13:06:58 +0200

    Create info endpoint

--- server.js
+++ server.js
@@ -2,8 +2,37 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const SYSINFO = {
+    studentName: 'Matthé van den Berg',
+    studentNumber: '2201635',
+    description: 'An API for storing and retrieving shared meals.'
+}
+
+app.use('*', (req, res, next) => {
+    const method = req.method
+    console.log(`Methode: ${method} has been called`)
+    next()
+})
+
+app.get('/api/info', (req, res) => {
+    res.status(200).json(
+        {
+            status: 200,
+            time: Date.now(),
+            message: 'Server info-endpoint',
+            data: SYSINFO
+        }
+    )
+})
+
+app.use('*', (req, res) => {
+    res.status(404).json(
+        {
+            status: 404,
+            message: 'Endpoint not found',
+            data: {}
+        }
+    )
 })
 
 app.listen(port, () => {
