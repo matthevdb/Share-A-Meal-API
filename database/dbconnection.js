7a5dad2a6b529a521a4c132dd149b168185c711c
Matthé <matthevdberg@gmail.com>
2023-05-05 12:14:51 +0200

    Add dbconnection
    Credentials are retrieved from .env file

---
+++ database/dbconnection.js
@@ -0,0 +1,12 @@
+var mysql = require("mysql2");
+var dotenv = require("dotenv").config();
+
+var pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+module.exports = pool;
