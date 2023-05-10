dc9f219576c47f4ea4e161ce5bfc52e206a1cb53
Matthé <matthevdberg@gmail.com>
2023-05-10 13:27:03 +0200

    Fix variable typo

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -4,7 +4,7 @@ var dotenv = require("dotenv").config();
 var pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.DB_HOST,
-  port: process.env.PORT,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
