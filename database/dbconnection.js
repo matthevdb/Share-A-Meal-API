649de2e9fe79498af142df5ed6544169f0fb221f
Matthé <matthevdberg@gmail.com>
2023-05-10 13:24:08 +0200

    Add missing PORT environment variable

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -4,6 +4,7 @@ var dotenv = require("dotenv").config();
 var pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.DB_HOST,
+  port: process.env.PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
