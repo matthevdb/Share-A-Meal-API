3e41e4b0e52d8fcded25a66b0612ed7d36451e84
Matthé <matthevdberg@gmail.com>
2023-05-16 14:40:50 +0200

    Add auth tests

---
+++ test/integration/auth.test.js
@@ -0,0 +1,111 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../../server");
+const pool = require("../../database/dbconnection");
+chai.should();
+chai.use(chaiHttp);
+
+const CLEAR_DATABASE =
+  "DELETE FROM meal_participants_user; DELETE FROM meal; DELETE FROM user; ALTER TABLE user AUTO_INCREMENT = 1";
+const INSERT_USER =
+  "INSERT INTO user (firstName, lastName, street, city, emailAdress, password, phoneNumber) " +
+  "VALUES ('Matthé', 'van den Berg', 'Lovensdijkstraat 61', 'Breda', 'm.vandenberg@avans.nl', 'Secret12', '06 12345678');";
+
+beforeEach((done) => {
+  pool.query(CLEAR_DATABASE, (err) => {
+    if (err) return done(err);
+    done();
+  });
+});
+
+after(() => {
+  pool.end();
+});
+
+describe("UC-101 Logging in", function () {
+  beforeEach((done) => {
+    pool.query(INSERT_USER, (error, result) => {
+      done();
+    });
+  });
+
+  it("TC-101-1 should return an error if a required field is missing", (done) => {
+    chai
+      .request(server)
+      .post("/api/login")
+      .send({
+        emailAddress: "m.vandenberg@avans.nl",
+      })
+      .end((err, res) => {
+        res.body.should.be.an("object");
+        res.body.should.has.property("status").to.be.equal(400);
+        res.body.should.has
+          .property("message")
+          .to.be.equal("password must be a string");
+        res.body.should.has.property("data").to.be.empty;
+        done();
+      });
+  });
+  it("TC-101-2 should return an error if password is invalid", (done) => {
+    chai
+      .request(server)
+      .post("/api/login")
+      .send({
+        emailAddress: "m.vandenberg@avans.nl",
+        password: "123",
+      })
+      .end((err, res) => {
+        res.body.should.be.an("object");
+        res.body.should.has.property("status").to.be.equal(400);
+        res.body.should.has
+          .property("message")
+          .to.be.equal("Emailaddress and password do not match");
+        res.body.should.has.property("data").to.be.empty;
+        done();
+      });
+  });
+  it("TC-101-3 should return an error if user does not exist", (done) => {
+    chai
+      .request(server)
+      .post("/api/login")
+      .send({
+        emailAddress: "n.vandenberg@avans.nl",
+        password: "Secret12",
+      })
+      .end((err, res) => {
+        res.body.should.be.an("object");
+        res.body.should.has.property("status").to.be.equal(404);
+        res.body.should.has
+          .property("message")
+          .to.be.equal("User does not exist");
+        res.body.should.has.property("data").to.be.empty;
+        done();
+      });
+  });
+  it("TC-101-4 should have successfully logged in the user", (done) => {
+    chai
+      .request(server)
+      .post("/api/login")
+      .send({
+        emailAddress: "m.vandenberg@avans.nl",
+        password: "Secret12",
+      })
+      .end((err, res) => {
+        res.body.should.be.an("object");
+        res.body.should.has.property("status").to.be.equal(200);
+        res.body.should.has.property("message").to.be.equal("User logged in");
+        res.body.should.has.property("data").to.include({
+          id: 1,
+          firstName: "Matthé",
+          lastName: "van den Berg",
+          street: "Lovensdijkstraat 61",
+          city: "Breda",
+          isActive: 1,
+          emailAdress: "m.vandenberg@avans.nl",
+          phoneNumber: "06 12345678",
+        });
+        res.body.data.should.have.property("token").to.be.a.string;
+        done();
+      });
+  });
+});
