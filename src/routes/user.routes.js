d94ca43ac4a4dcc18d0daab72c4540ca1b55ccdd
Matthé <matthevdberg@gmail.com>
2023-05-17 09:25:49 +0200

    Add token validation to user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,22 +7,39 @@ const authController = require("../controllers/auth.controller");
 router.post("/api/user", userController.validateUser, userController.addUser);
 
 // UC-202 Opvragen van overzicht van users
-router.get("/api/user", userController.getAllUsers);
+router.get(
+  "/api/user",
+  authController.validateToken,
+  userController.getAllUsers
+);
 
 // UC-203 Opvragen van gebruikersprofiel
-router.get("/api/user/profile", userController.getUserProfile);
+router.get(
+  "/api/user/profile",
+  authController.validateToken,
+  userController.getUserProfile
+);
 
 // UC-204 Opvragen van usergegevens bij ID
-router.get("/api/user/:id", userController.getUserByID);
+router.get(
+  "/api/user/:id",
+  authController.validateToken,
+  userController.getUserByID
+);
 
 // UC-205 Wijzigen van usergegevens
 router.put(
   "/api/user/:id",
+  authController.validateToken,
   userController.validateUser,
   userController.updateUserData
 );
 
 // UC-206 Verwijderen van user
-router.delete("/api/user/:id", userController.deleteUser);
+router.delete(
+  "/api/user/:id",
+  authController.validateToken,
+  userController.deleteUser
+);
 
 module.exports = router;
