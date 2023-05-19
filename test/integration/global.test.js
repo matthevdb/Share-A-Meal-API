b24cefdd53bd954a84c5d054f40ee5af642aefc0
Matthé <matthevdberg@gmail.com>
2023-05-19 09:52:41 +0200

    Add auto_increment reset for meal ids

--- test/integration/global.test.js
+++ test/integration/global.test.js
@@ -1,7 +1,7 @@
 const pool = require("../../database/dbconnection");
 
 const CLEAR_DATABASE =
-  "DELETE FROM meal_participants_user; DELETE FROM meal; DELETE FROM user; ALTER TABLE user AUTO_INCREMENT = 1";
+  "DELETE FROM meal_participants_user; DELETE FROM meal; DELETE FROM user; ALTER TABLE user AUTO_INCREMENT = 1; ALTER TABLE meal AUTO_INCREMENT = 1";
 
 beforeEach((done) => {
   pool.query(CLEAR_DATABASE, () => {
